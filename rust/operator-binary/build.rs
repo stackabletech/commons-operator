33823b00fe250b3959b9cdf7a6ac6213f39696c9
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-03-22 15:15:42 +0100

    Add AuthenticationClass

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,3 +1,8 @@
+use stackable_commons_crd::authentication::AuthenticationClass;
+use stackable_operator::crd::CustomResourceExt;
+
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
+
+    AuthenticationClass::write_yaml_schema("../../deploy/crd/authentication.crd.yaml").unwrap();
 }
