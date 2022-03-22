092a2fdb68152ec067c91e85e5b8f074627604d9
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-03-22 13:00:12 +0100

    Initial crate structure

---
+++ rust/operator-binary/src/main.rs
@@ -0,0 +1,23 @@
+use stackable_operator::cli::Command;
+
+use clap::Parser;
+
+mod built_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+#[derive(Parser)]
+#[clap(about = built_info::PKG_DESCRIPTION, author = stackable_operator::cli::AUTHOR)]
+struct Opts {
+    #[clap(subcommand)]
+    cmd: Command,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    stackable_operator::logging::initialize_logging("COMMONS_OPERATOR_LOG");
+
+    let opts = Opts::parse();
+
+    Ok(())
+}
