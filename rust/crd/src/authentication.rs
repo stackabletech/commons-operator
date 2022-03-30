fe1567b7a1437e6c2d3fa779a57e8dc5f70441c2
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-03-30 08:22:48 +0200

    Set default for LdapFieldNames

--- rust/crd/src/authentication.rs
+++ rust/crd/src/authentication.rs
@@ -43,7 +43,8 @@ pub struct LdapAuthenticationProvider {
     #[serde(default)]
     pub search_filter: String,
     /// The name of the LDAP object fields
-    pub ldap_field_names: Option<LdapFieldNames>,
+    #[serde(default)]
+    pub ldap_field_names: LdapFieldNames,
     /// In case you need a special account for searching the LDAP server you can specify it here
     pub bind_credentials: Option<SecretClassVolume>,
     /// Use a TLS connection. If not specified no TLS will be used
@@ -69,11 +70,11 @@ pub struct LdapFieldNames {
     #[serde(default = "LdapFieldNames::default_group")]
     pub group: String,
     /// The name of the firstname field
-    #[serde(default = "LdapFieldNames::default_firstname")]
-    pub firstname: String,
+    #[serde(default = "LdapFieldNames::default_given_name")]
+    pub given_name: String,
     /// The name of the lastname field
-    #[serde(default = "LdapFieldNames::default_lastname")]
-    pub lastname: String,
+    #[serde(default = "LdapFieldNames::default_surname")]
+    pub surname: String,
     /// The name of the email field
     #[serde(default = "LdapFieldNames::default_email")]
     pub email: String,
@@ -88,15 +89,27 @@ impl LdapFieldNames {
         "memberof".to_string()
     }
 
-    fn default_firstname() -> String {
+    fn default_given_name() -> String {
         "givenName".to_string()
     }
 
-    fn default_lastname() -> String {
+    fn default_surname() -> String {
         "sn".to_string()
     }
 
     fn default_email() -> String {
         "mail".to_string()
+    }
+}
+
+impl Default for LdapFieldNames {
+    fn default() -> Self {
+        LdapFieldNames{
+            uid: Self::default_uid(),
+            group: Self::default_group(),
+            given_name: Self::default_given_name(),
+            surname: Self::default_surname(),
+            email: Self::default_email(),
+        }
     }
 }
