7d99f8ac7be96d3101782dbc4708520c7f6f8ffb
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2025-08-21 11:04:47 +0200

    chore: Update to stackable-operator 0.95.0 (#374)

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -43,15 +43,15 @@ async fn main() -> anyhow::Result<()> {
         Command::Run(ProductOperatorRun {
             product_config: _,
             watch_namespace,
-            telemetry_arguments,
-            cluster_info_opts,
+            operator_environment: _,
+            telemetry,
+            cluster_info,
         }) => {
             // NOTE (@NickLarsenNZ): Before stackable-telemetry was used:
             // - The console log level was set by `COMMONS_OPERATOR_LOG`, and is now `CONSOLE_LOG` (when using Tracing::pre_configured).
             // - The file log level was (maybe?) set by `COMMONS_OPERATOR_LOG`, and is now set via `FILE_LOG` (when using Tracing::pre_configured).
             // - The file log directory was set by `COMMONS_OPERATOR_LOG_DIRECTORY`, and is now set by `ROLLING_LOGS_DIR` (or via `--rolling-logs <DIRECTORY>`).
-            let _tracing_guard =
-                Tracing::pre_configured(built_info::PKG_NAME, telemetry_arguments).init()?;
+            let _tracing_guard = Tracing::pre_configured(built_info::PKG_NAME, telemetry).init()?;
 
             tracing::info!(
                 built_info.pkg_version = built_info::PKG_VERSION,
@@ -65,7 +65,7 @@ async fn main() -> anyhow::Result<()> {
 
             let client = stackable_operator::client::initialize_operator(
                 Some("commons.stackable.tech".to_string()),
-                &cluster_info_opts,
+                &cluster_info,
             )
             .await?;
 
